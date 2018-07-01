2d51601a3abcadbc8fea165d9f20d0a180e6884b
Feng Zhu <lyricorpse@gmail.com>
2018-07-01 10:36:11 -0700

    Modify config for build_ye_files

--- LMR_prior.py
+++ LMR_prior.py
@@ -3,41 +3,41 @@ Module: LMR_prior.py
 
 Purpose: Contains definitions of classes defining the various sources
          (i.e. model simulations and reanalyses) which may be used as to
-         populate the prior in the LMR. Also contains the code used to 
+         populate the prior in the LMR. Also contains the code used to
          randomly pick model states along the temporal domain to populate
-         the prior ensemble. 
+         the prior ensemble.
 
 Originator: Robert Tardif | Dept. of Atmospheric Sciences, Univ. of Washington
                           | January 2015
 
-Revisions: 
-          - Added the ERA20CM (ECMWF 20th century ensemble simulation) as a 
+Revisions:
+          - Added the ERA20CM (ECMWF 20th century ensemble simulation) as a
             possible source of prior data to be used by the LMR.
             [R. Tardif, U. of Washington, December 2015]
           - Added the option of detrending the prior
             [R. Tardif, U. of Washington, March 2016]
           - Added the 'ccsm4_isotope_controlrun' as a possible prior source.
-            Contains simulated isotope (d18O) field. 
+            Contains simulated isotope (d18O) field.
             [R. Tardif, U. of Washington, May 2016]
-          - Added 'ccsm3_trace21ka' (simulation of the transient climate of 
+          - Added 'ccsm3_trace21ka' (simulation of the transient climate of
             the last 21k years i.e. LGM -> Holocene) as a possible prior source
             [R. Tardif, U. of Washington, Nov 2016]
           - Added the 'loveclim_goosse2005' class (Common Era simulation performed
-            with the LOVECLIM v1.0 model by Goosse et al. GRL 2005) as a 
+            with the LOVECLIM v1.0 model by Goosse et al. GRL 2005) as a
             possible prior source.
             [R. Tardif, U. of Washington, Jul 2017]
-          - Added the 'cgenie_petm' class (simulations of the PETM with the 
+          - Added the 'cgenie_petm' class (simulations of the PETM with the
             cGENIE EMIC) as a possible prior source.
             [R. Tardif, U. of Washington, Aug 2017]
-          - Added the 'ihadcm3_preindustrial_control' class for use of data from 
-            the isotope-enabled HadCM3 model simulation of preindustrial climate 
-            (the "0kyr" run, part of paleoclimate simulations performed by 
+          - Added the 'ihadcm3_preindustrial_control' class for use of data from
+            the isotope-enabled HadCM3 model simulation of preindustrial climate
+            (the "0kyr" run, part of paleoclimate simulations performed by
             Max Holloway of the British Antarctic Survey)
             [R. Tardif, U. of Washington, Aug 2017]
           - Added the 'icesm_last_millennium' and 'icesm_last_millennium_historical'
-            classes for isotope-enabled CESM model simulations of the 
-            last millennium (0850 to 1850) and of the last millennium to which 
-            a modern historical simulation (1850 to 2006) has been concatenated. 
+            classes for isotope-enabled CESM model simulations of the
+            last millennium (0850 to 1850) and of the last millennium to which
+            a modern historical simulation (1850 to 2006) has been concatenated.
             [R. Tardif, U. of Washington, Nov 2017]
 
 """
@@ -76,17 +76,17 @@ def prior_assignment(iprior):
     elif iprior == 'icesm_last_millennium':
         prior_object = prior_icesm_last_millennium()
     elif iprior == 'icesm_last_millennium_historical':
-        prior_object = prior_icesm_last_millennium_historical()        
+        prior_object = prior_icesm_last_millennium_historical()
     elif iprior == 'ihadcm3_preindustrial_control':
         prior_object = prior_ihadcm3_preindustrial_control()
     elif iprior == 'ccsm3_trace21ka':
         prior_object = prior_ccsm3_trace21ka()
     elif iprior == 'cgenie_petm':
         prior_object = prior_cgenie_petm()
-    elif iprior == 'cesm_last_millennium':
-        prior_object = prior_cesm_last_millennium()
+    elif iprior == 'cesm_last_millenium':
+        prior_object = prior_cesm_last_millenium()
 
-        
+
     return prior_object
 
 
@@ -102,24 +102,24 @@ class prior_master(object):
     def populate_ensemble(self,prior_source, prior_cfg):
 
         # Load prior data from file(s) - multiple state variables
-        self.read_prior()        
-        
+        self.read_prior()
+
         Nens_max = len(self.prior_dict[list(self.prior_dict.keys())[0]]['years'])
         if self.Nens and self.Nens > Nens_max:
             raise SystemExit('ERROR in populate_ensemble! Specified ensemble size too large for available nb of states. '
             'Max allowed with current configuration: %d' %Nens_max)
 
-        
+
         nbvars = len(self.statevars)
         # Check consistency between specified state variables and uploaded dictionary
         if len(list(self.prior_dict.keys())) != nbvars:
             raise SystemExit('Problem with load of prior state variables. Exiting!')
 
-        
+
         # Defining content of state vector => dictionary: state_vect_content
         # NOTE: now assumes that dims of state variables are (lat,lon) only !!!
         state_vect_info = {}
-        
+
         # Loop over state variables
         Nx = 0
         timedim = []
@@ -142,7 +142,7 @@ class prior_master(object):
                     # each be defined with a 2d array
                     ndim1 = self.prior_dict[var][dim1].shape[0]
                     ndim2 = self.prior_dict[var][dim1].shape[1]
-                    
+
                 elif len(self.prior_dict[var][dim1].shape) == 1 and len(self.prior_dict[var][dim2].shape) == 1:
                     # regular lat/lon array : lat and lon can be defined with 1d arrays
                     ndim1 = len(self.prior_dict[var][dim1])
@@ -152,7 +152,7 @@ class prior_master(object):
                     raise SystemExit('ERROR in populate_ensemble: Unrecognized info on spatial dimensions. Exiting!')
 
                 ndimtot = ndim1*ndim2
-                
+
                 dct['pos'] = (Nx,Nx+(ndimtot)-1)
                 dct['spacecoords'] = spacecoords
                 dct['spacedims'] = (ndim1,ndim2)
@@ -163,7 +163,7 @@ class prior_master(object):
                     dct['vartype'] = '2D:meridional_vertical'
                 else:
                     raise SystemExit('ERROR in populate_ensemble: Unrecognized info on spatial dimensions. Exiting!')
-                
+
             elif vartype == '1D:meridional':
                 dim1,  = spacecoords
                 ndim1 = self.prior_dict[var][dim1].shape[0]
@@ -172,7 +172,7 @@ class prior_master(object):
                 dct['spacecoords'] = spacecoords
                 dct['spacedims'] = (ndim1,)
                 dct['vartype'] = vartype
-                
+
             else:
                 # variable is simple time series'
                 ndimtot = 1
@@ -180,7 +180,7 @@ class prior_master(object):
                 dct['spacecoords'] = None
                 dct['spacedims'] = None
                 dct['vartype'] = '0D:time series'
-                
+
             # assign to master dictionary
             state_vect_info[var] = dct
 
@@ -192,7 +192,7 @@ class prior_master(object):
         print('State vector information:')
         print('Nx =', Nx)
         print('state_vect_info=', state_vect_info)
-        
+
         # time dimension consistent across variables?
         if all(x == timedim[0] for x in timedim):
             ntime = timedim[0]
@@ -228,7 +228,7 @@ class prior_master(object):
         for var in list(self.prior_dict.keys()):
 
             vartype = self.prior_dict[var]['vartype']
-            
+
             indstart = state_vect_info[var]['pos'][0]
             indend   = state_vect_info[var]['pos'][1]
 
@@ -239,7 +239,7 @@ class prior_master(object):
 
                     # get the name of the spatial coordinates for state variable 'var'
                     coordname1, coordname2 = state_vect_info[var]['spacecoords']
-                    # load in the coord values from data dictionary 
+                    # load in the coord values from data dictionary
                     coord1 = self.prior_dict[var][coordname1]
                     coord2 = self.prior_dict[var][coordname2]
 
@@ -271,19 +271,19 @@ class prior_master(object):
 
                     # get the name of the spatial coordinate for state variable 'var'
                     coordname1, = state_vect_info[var]['spacecoords']
-                    # load in the coord values from data dictionary 
+                    # load in the coord values from data dictionary
                     X_coord1 = self.prior_dict[var][coordname1]
 
                     Xb_coords[indstart:indend+1,0] = X_coord1.flatten()
 
                     # Some cleanup
-                    del X_coord1                    
+                    del X_coord1
 
             elif vartype == '0D:time series':
                 # Loop over ensemble members
                 for i in range(0,self.Nens):
                     Xb[indstart:indend+1,i] = self.prior_dict[var]['value'][ind_ens[i]].flatten()
-                
+
             else:
                 raise SystemExit('ERROR im populate_ensemble: variable of unrecognized spatial dimensions. Exiting!')
 
@@ -292,7 +292,7 @@ class prior_master(object):
             """
             # RT dev ... ... ...
             # if anom_reference period is defined, re-centering sample around a mean of zero ? ...
-            
+
             print self.prior_dict[var]['climo'].shape
             print self.prior_dict[var]['value'][ind_ens[0]].shape
 
@@ -300,15 +300,15 @@ class prior_master(object):
             print np.mean(Xb[indstart:indend+1,:], axis=1)
 
             sample_mean = np.mean(Xb[indstart:indend+1,:], axis=1)
-            
+
             Xb[indstart:indend+1,:] = Xb[indstart:indend+1,:] - sample_mean[:,np.newaxis]
 
             print np.mean(Xb[indstart:indend+1,:], axis=1)
-            
+
             exit(1)
             # RT dev ... ... ...
             """
-        
+
         # Returning state vector Xb as masked array, if it contains
         # at least one invalid value
 
@@ -318,18 +318,18 @@ class prior_master(object):
 
             # Set fill_value to np.nan
             np.ma.set_fill_value(Xb_res, np.nan)
-        
+
             # array indices of masked & valid elements
             inds_mask = np.nonzero(Xb_res.mask)
             inds_valid = np.nonzero(~Xb_res.mask)
         else:
             Xb_res = Xb
-        
+
         # Assign return variables
         self.ens = Xb_res
         self.coords = Xb_coords
         self.full_state_info = state_vect_info
-        
+
         return
 
 
@@ -354,7 +354,7 @@ class prior_BerkeleyEarth(prior_master):
 class prior_ccsm4_last_millenium(prior_master):
 
     def read_prior(self):
-    
+
         from load_gridded_data import read_gridded_data_CMIP5_model
         self.prior_dict = read_gridded_data_CMIP5_model(self.prior_datadir,
                                                         self.prior_datafile,
@@ -392,7 +392,7 @@ class prior_ccsm4_isotope_controlrun(prior_master):
                                                         self.anom_reference,
                                                         self.statevars_info)
         return
-    
+
 # class for the MPI-ESM-P Last Millenniun simulation
 class prior_mpi_esm_p_last_millenium(prior_master):
 
@@ -463,7 +463,7 @@ class prior_era20cm(prior_master):
 class prior_loveclim_goosse2005(prior_master):
 
     def read_prior(self):
-    
+
         from load_gridded_data import read_gridded_data_CMIP5_model
         self.prior_dict = read_gridded_data_CMIP5_model(self.prior_datadir,
                                                         self.prior_datafile,
@@ -478,7 +478,7 @@ class prior_loveclim_goosse2005(prior_master):
 class prior_icesm_last_millennium(prior_master):
 
     def read_prior(self):
-    
+
         from load_gridded_data import read_gridded_data_CMIP5_model
         self.prior_dict = read_gridded_data_CMIP5_model(self.prior_datadir,
                                                         self.prior_datafile,
@@ -493,7 +493,7 @@ class prior_icesm_last_millennium(prior_master):
 class prior_icesm_last_millennium_historical(prior_master):
 
     def read_prior(self):
-    
+
         from load_gridded_data import read_gridded_data_CMIP5_model
         self.prior_dict = read_gridded_data_CMIP5_model(self.prior_datadir,
                                                         self.prior_datafile,
@@ -509,7 +509,7 @@ class prior_icesm_last_millennium_historical(prior_master):
 class prior_ihadcm3_preindustrial_control(prior_master):
 
     def read_prior(self):
-    
+
         from load_gridded_data import read_gridded_data_CMIP5_model
         self.prior_dict = read_gridded_data_CMIP5_model(self.prior_datadir,
                                                         self.prior_datafile,
@@ -533,7 +533,7 @@ class prior_ccsm3_trace21ka(prior_master):
                                                       self.anom_reference)
         return
 
-    
+
 # class for the cGENIE simulation if the PETM
 class prior_cgenie_petm(prior_master):
 
@@ -548,7 +548,7 @@ class prior_cgenie_petm(prior_master):
         return
 
 # class for the CESM last millennium simulation
-class prior_cesm_last_millennium(prior_master):
+class prior_cesm_last_millenium(prior_master):
 
     def read_prior(self):
 
